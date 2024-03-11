9008a88682900da8f22ead69c90d9e0c12984735
Khanh Nguyen <nhkk49@gmail.com>
2024-03-11 00:13:08 -0400

    update nvim

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -1,45 +1,13 @@
 return {
 	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons",
 		"MunifTanjim/nui.nvim",
 	},
-	keys = {
-		{ "<leader>fe", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
-	},
 	config = function()
-		require("neo-tree").setup({
-			close_if_last_window = true,
-			enable_git_status = true,
-			enable_diagnostics = true,
-			window = {
-				position = "right",
-			},
-			default_component_configs = {
-				indent = {
-					indent_marker = "│",
-				},
-				modified = {
-					symbol = "[+]",
-					highlight = "NeoTreeModified",
-				},
-				git_status = {
-					symbols = {
-						-- Change type
-						added = "✚",
-						modified = "",
-						deleted = "✖",
-						renamed = "",
-						-- Status type
-						untracked = "",
-						ignored = "",
-						unstaged = "",
-						staged = "",
-						conflict = "",
-					},
-				},
-			},
-		})
+		vim.keymap.set("n", "<leader>fe", ":Neotree filesystem reveal right<CR>", {})
+		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
 	end,
 }
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,52 +1,24 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		{
-			"nvim-telescope/telescope-fzf-native.nvim",
-			build = "make",
-		},
-		"debugloop/telescope-undo.nvim",
-	},
-	cmd = "Telescope",
-	keys = {
-		{ "<leader>ff", "<cmd>:Telescope find_files<CR>", { "n" }, desc = "Telescope files" },
-		{ "<leader>fg", "<cmd>:Telescope live_grep<CR>", { "n" }, desc = "Telescope grep" },
-		{ "<leader>fb", "<cmd>:Telescope buffers<CR>", { "n" }, desc = "Telescope buffers" },
-		{ "<leader>fh", "<cmd>:Telescope help_tags<CR>", { "n" }, desc = "Telescope help" },
-		{ "<leader>fb", "<cmd>:Telescope buffers<CR>", { "n" }, desc = "Telescope buffer" },
-		{ "<leader>fu", "<cmd>:Telescope undo<CR>", { "n" }, desc = "Telescope undo" },
-	},
-	opts = {
-		defaults = {
-			path_display = { "smart" },
-			file_ignore_patterns = { ".git/" },
-		},
-		pickers = {
-			find_files = {
-				find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
-			},
-		},
-		extensions = {
-			fzf = {
-				-- theme = "jellybeans",
-				fuzzy = true,
-				override_generic_sorter = true,
-				override_file_sorter = true,
-				case_mode = "smart_case",
-			},
-			undo = {
-				side_by_side = true,
-				layout_strategy = "vertical",
-				layout_config = {
-					preview_height = 0.8,
-				},
-			},
-		},
-	},
-	config = function()
-		-- Load the extensions
-		require("telescope").load_extension("fzf")
-		require("telescope").load_extension("undo")
-	end,
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+
+      require("telescope").load_extension("ui-select")
+    end,
+  },
 }
