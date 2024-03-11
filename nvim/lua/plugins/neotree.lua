302b1b96ae93d8d73209dbaf03c508c826969119
Khanh Nguyen <nhkk49@gmail.com>
2024-03-11 00:19:29 -0400

    update neotree

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -6,8 +6,42 @@ return {
 		"nvim-tree/nvim-web-devicons",
 		"MunifTanjim/nui.nvim",
 	},
+  keys = {
+    { "<leader>fe", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
+  },
 	config = function()
-		vim.keymap.set("n", "<leader>fe", ":Neotree filesystem reveal right<CR>", {})
-		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+    require("neo-tree").setup({
+ 			close_if_last_window = true,
+ 			enable_git_status = true,
+ 			enable_diagnostics = true,
+ 			window = {
+ 				position = "right",
+ 			},
+ 			default_component_configs = {
+ 				indent = {
+ 					indent_marker = "│",
+ 				},
+ 				modified = {
+ 					symbol = "[+]",
+ 					highlight = "NeoTreeModified",
+ 				},
+ 				git_status = {
+ 					symbols = {
+ 						-- Change type
+ 						added = "✚",
+ 						modified = "",
+ 						deleted = "✖",
+ 						renamed = "",
+ 						-- Status type
+ 						untracked = "",
+ 						ignored = "",
+ 						unstaged = "",
+ 						staged = "",
+ 						conflict = "",
+ 					},
+ 				},
+ 			},
+ 		})
 	end,
 }
+
